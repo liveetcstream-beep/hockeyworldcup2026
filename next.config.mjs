637d7e549c69e9ff767267e7981fd26bfd760609
Build User <build@local.com>
2026-07-08 23:25:51 +0500

    seo: Update navigation anchor texts to 'Hockey Live Streaming' and setup 301 redirects in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/broadcasters',
+        destination: '/hockey-live-streaming',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
